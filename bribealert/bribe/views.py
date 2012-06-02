3101011813fc6b14727a0c6396e78361dbeb69c4
Michael Hopstock <michael.hopstock@student.hpi.uni-potsdam.de>
2012-06-02 18:40:02 +0200

    csrf token removed from upload

--- bribealert/bribe/views.py
+++ bribealert/bribe/views.py
@@ -1,15 +1,18 @@
 import simplejson
 
 from django.http import HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
+
 from forms import BribeForm
 
+@csrf_exempt
 def upload(request):
-	form = BribeForm(request.POST or None, request.FILES or None)
+    form = BribeForm(request.POST or None, request.FILES or None)
 
-	if form.is_valid():
-		new_bribe = form.save()
-		response = simplejson.dumps({'id': new_bribe.id})
-		
-		return HttpResponseRedirect(response, mimetype='application/json')
+    if form.is_valid():
+        new_bribe = form.save()
+        response = simplejson.dumps({'id': new_bribe.id})
+        
+        return HttpResponseRedirect(response, mimetype='application/json')
 
-	return HttpResponseBadRequest()
+    return HttpResponseBadRequest()
