0116dd50135325249765ea7f46377bfe0ff9c196
Robert Strobl <robert.strobl@student.hpi.uni-potsdam.de>
2012-06-02 21:27:59 +0200

    added auto_generated secure token and other model fields for bribes

--- bribealert/bribe/models.py
+++ bribealert/bribe/models.py
@@ -1,8 +1,18 @@
+import string
+import random
+
 from django.db import models
+from django.conf import settings
 
 class Bribe(models.Model):
-	lon = models.FloatField()
-	lat = models.FloatField()
-	date = models.DateTimeField()
-	secure_token = models.CharField(max_length=32, unique=True)
-	record = models.FileField(upload_to='records/')
+    lon = models.FloatField()
+    lat = models.FloatField()
+    date = models.DateTimeField()
+    secure_token = models.CharField(max_length=32, unique=True, blank=True)
+    record = models.FileField(upload_to='records/')
+    published = models.BooleanField(default=False)
+    
+    def save(self, *args, **kwargs):        
+        self.secure_token = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(settings.SECURE_TOKEN_LENGTH))
+
+        super(Bribe, self).save(*args, **kwargs)
