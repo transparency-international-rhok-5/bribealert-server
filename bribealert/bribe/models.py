b8c30c3bc1d9b7ee39ebf15200f15875a9092342
Robert Strobl <robert.strobl@student.hpi.uni-potsdam.de>
2012-06-03 12:10:13 +0200

    supporting videos

--- bribealert/bribe/models.py
+++ bribealert/bribe/models.py
@@ -23,8 +23,9 @@ class Bribe(models.Model):
     country = models.ForeignKey('Country', blank=True, null=True)
     date = models.DateTimeField()
     secure_token = models.CharField(max_length=32, unique=True, blank=True)
-    record = models.FileField(upload_to='records/')
-    description = models.TextField(blank=True)
+    audio_record = models.FileField(upload_to='records/', null=True)
+    video_record = models.FileField(upload_to='records/', null=True)
+    description = models.TextField(blank=True, null=True)
     published = models.BooleanField(default=False)
     
     objects = BribeManager()
